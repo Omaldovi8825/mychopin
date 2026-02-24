a7ddfda5225eb96bbcccad634809d687012200dd
Omar Maldonado <omaldonado@apexsystems.com>
2026-02-24 15:17:20 -0600

    agregue prettier

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
-import express from "express"
-import cors from "cors"
-import listasRouter from "./routes/lista.js"
-import mongoConnection from "./db/connection.js"
+import express from 'express'
+import cors from 'cors'
+import listasRouter from './routes/lista.js'
+import mongoConnection from './db/connection.js'
 
 const PORT = 3000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/api", listasRouter)
+app.use('/api', listasRouter)
 
 async function startServer() {
   try {
@@ -18,21 +18,21 @@ async function startServer() {
       console.log(`Server running on http://localhost:${PORT}`)
     })
   } catch (err) {
-    console.error("Fallo conexión a MongoDB", err)
+    console.error('Fallo conexión a MongoDB', err)
     process.exit(1) // termina el proceso si no se puede conectar
   }
 }
 
 startServer()
 
-process.on("SIGINT", async () => {
-  console.log("Cerrando conexión a MongoDB...")
+process.on('SIGINT', async () => {
+  console.log('Cerrando conexión a MongoDB...')
   await mongoConnection.close()
   process.exit(0)
 })
 
-process.on("SIGTERM", async () => {
-  console.log("Cerrando conexión a MongoDB...")
+process.on('SIGTERM', async () => {
+  console.log('Cerrando conexión a MongoDB...')
   await mongoConnection.close()
   process.exit(0)
 })
