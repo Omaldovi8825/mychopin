59c0d59ebbe7fc9c3cd5f8bbdfc9064f778fe6f9
Omar Maldonado <omaldonado@apexsystems.com>
2026-02-23 22:09:49 -0600

    agregue validacion cors

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import apiRoutes from "./routes/lista.js"
 import mongoConnection from "./db/connection.js"
 
@@ -8,6 +9,7 @@ dotenv.config()
 const PORT = 3000
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use("/api", apiRoutes)
 
