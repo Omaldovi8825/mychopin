ad6cb514ac90e86eed02ebcd0b5eb765fc7c1535
Omaldovi8825 <70729609+Omaldovi8825@users.noreply.github.com>
2026-02-23 21:44:36 -0600

    se agrego conexion a mongo (#4)
    Co-authored-by: Omar Maldonado <omaldonado@apexsystems.com>

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,39 @@
 import express from "express"
+import dotenv from "dotenv"
 import apiRoutes from "./routes/lista.js"
+import mongoConnection from "./db/connection.js"
+
+dotenv.config()
 
 const PORT = 3000
 const app = express()
 
+app.use(express.json())
 app.use("/api", apiRoutes)
 
-app.listen(PORT, () => {
-  console.log(`server encendido en puerto ${PORT}`)
+async function startServer() {
+  try {
+    await mongoConnection.connect()
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`)
+    })
+  } catch (err) {
+    console.error("Fallo conexión a MongoDB", err)
+    process.exit(1) // termina el proceso si no se puede conectar
+  }
+}
+
+startServer()
+
+process.on("SIGINT", async () => {
+  console.log("Cerrando conexión a MongoDB...")
+  await mongoConnection.close()
+  process.exit(0)
+})
+
+process.on("SIGTERM", async () => {
+  console.log("Cerrando conexión a MongoDB...")
+  await mongoConnection.close()
+  process.exit(0)
 })
