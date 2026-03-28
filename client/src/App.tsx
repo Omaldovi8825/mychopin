ccefe74f1acc11f402dffa34a204a9dd30156f11
Efren Gutierrez Torres <efrengutierreztorres@gmail.com>
2026-03-27 18:38:00 -0600

    agregue logica para guardar lista, pero tiene error de renderizado que no se resolver

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import ListasRecientes from './components/ListasRecientes'
 import NuevaLista from './components/NuevaLista'
+import type { Lista } from './types/lista'
+
 import { useState, useEffect } from 'react'
 import { Api } from './utils/ApiCalls'
 
 function App() {
-  const [listasRecientes, setListasRecientes] = useState([])
+  const [listasRecientes, setListasRecientes] = useState<Lista[]>([])
 
   const traerListas = async () => {
     try {
       const res = await Api.get('listas')
-      setListasRecientes(res.data)
+      setListasRecientes(res.data.data)
     } catch (error) {
       console.error(error)
     }
@@ -19,9 +21,18 @@ function App() {
     traerListas()
   }, [])
 
+  const guardarLista = async (nuevaLista: string[]) => {
+    try {
+      await Api.post('listas', nuevaLista)
+      traerListas()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="container">
-      <NuevaLista />
+      <NuevaLista guardarLista={guardarLista} />
       <ListasRecientes listasRecientes={listasRecientes} />
     </div>
   )
